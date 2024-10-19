eaccac9ec2f110545a8c27bc7bdbcf23dc8552b1
MarcinMiszta <157218780+MarcinMiszta@users.noreply.github.com>
2024-10-19 13:57:59 +0200

    Dodaj pliki projektów.

---
+++ cw1/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace cw1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(93, 164);
+            button1.Margin = new Padding(3, 4, 3, 4);
+            button1.Name = "button1";
+            button1.Size = new Size(135, 157);
+            button1.TabIndex = 0;
+            button1.Text = "Sortowanie bąbelkowe";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(256, 164);
+            button2.Margin = new Padding(3, 4, 3, 4);
+            button2.Name = "button2";
+            button2.Size = new Size(135, 157);
+            button2.TabIndex = 2;
+            button2.Text = "Sortowanie przez wstawianie";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(419, 164);
+            button3.Margin = new Padding(3, 4, 3, 4);
+            button3.Name = "button3";
+            button3.Size = new Size(135, 157);
+            button3.TabIndex = 3;
+            button3.Text = "Sortowanie przez scalenie";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(582, 164);
+            button4.Margin = new Padding(3, 4, 3, 4);
+            button4.Name = "button4";
+            button4.Size = new Size(135, 157);
+            button4.TabIndex = 4;
+            button4.Text = "Sortowanie szybkie";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(743, 164);
+            button5.Margin = new Padding(3, 4, 3, 4);
+            button5.Name = "button5";
+            button5.Size = new Size(135, 157);
+            button5.TabIndex = 5;
+            button5.Text = "Sortowanie przez zliczanie";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1080, 622);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Margin = new Padding(3, 4, 3, 4);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+    }
+}
---
+++ cw1/Form1.cs
@@ -0,0 +1,215 @@
+using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace cw1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        string ToString(int[] tab)
+        {
+            string wynik = "";
+            for (int i = 0; i < tab.Length; i++)
+            {
+                wynik += tab[i] + " ";
+            }
+            return wynik;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int[] tab = { 8, 7, 1, 3, 5, 2, 4 };
+
+            for (int i = 0; i < tab.Length - 1; i++)
+            {
+                for (int j = 0; j < tab.Length - 1; j++)
+                {
+                    int temp = tab[j];
+                    if (tab[j] > tab[j + 1])
+                    {
+                        tab[j] = tab[j + 1];
+                        tab[j + 1] = temp;
+                    }
+                }
+            }
+
+            string message = string.Join(",", tab);
+            MessageBox.Show(message);
+
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int[] tab = { 8, 7, 1, 3, 5, 2, 4 };
+            for (int i = 1; i < tab.Length; i++)
+            {
+                int j = i - 1;
+                int temp = tab[i];
+                while (j >= 0 && tab[j] > temp)
+                {
+                    tab[j + 1] = tab[j];
+                    j--;
+                }
+                tab[j + 1] = temp;
+            }
+            string message = string.Join(",", tab);
+            MessageBox.Show(message);
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int[] tab = { 8, 7, 1, 3, 5, 2, 4 };
+            MergeSort(tab);
+            static void MergeSort(int[] tab)
+            {
+                if (tab.Length <= 1)
+                {
+                    return;
+                }
+                int middle = tab.Length / 2;
+                int[] side1 = new int[middle];
+                int[] side2 = new int[tab.Length - middle];
+                for (int i = 0; i < middle; i++)
+                {
+                    side1[i] = tab[i];
+                }
+                for (int i = middle; i < tab.Length; i++)
+                {
+                    side2[i - middle] = tab[i];
+                }
+                MergeSort(side1);
+                MergeSort(side2);
+                sort(tab, side1, side2);
+            }
+            static void sort(int[] tab, int[] side1, int[] side2)
+            {
+                int i = 0, j = 0, k = 0;
+
+                while (i < side1.Length && j < side2.Length)
+                {
+                    if (side1[i] < side2[j])
+                    {
+                        tab[k++] = side1[i++];
+                    }
+                    else
+                    {
+                        tab[k++] = side2[j++];
+                    }
+                }
+
+                while (i < side1.Length)
+                {
+                    tab[k++] = side1[i++];
+                }
+
+                while (j < side2.Length)
+                {
+                    tab[k++] = side2[j++];
+
+                }
+            }
+            string message = string.Join(",", tab);
+            MessageBox.Show(message);
+        }
+
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int[] tab = { 8, 7, 1, 3, 5, 2, 4 };
+            QuickSort(tab, 0, tab.Length - 1);
+
+            static void QuickSort(int[] tab, int leftIndex, int rightIndex)
+            {
+                if (rightIndex <= leftIndex) return;
+
+                int i = leftIndex;
+                int j = rightIndex;
+                int pivot = tab[(leftIndex + rightIndex) / 2];
+
+                while (i <= j)
+                {
+                    while (i <= rightIndex && tab[i] < pivot) i++;
+                    while (j >= leftIndex && tab[j] > pivot) j--;
+
+                    if (i <= j)
+                    {
+                        Swap(ref tab[i], ref tab[j]);
+                        i++;
+                        j--;
+                    }
+                }
+
+                if (leftIndex < j)
+                {
+                    QuickSort(tab, leftIndex, j);
+                }
+                if (i < rightIndex)
+                {
+                    QuickSort(tab, i, rightIndex);
+                }
+            }
+
+            static void Swap(ref int a, ref int b)
+            {
+                int temp = a;
+                a = b;
+                b = temp;
+            }
+
+            string message = string.Join(",", tab);
+            MessageBox.Show(message);
+        }
+
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            int[] tab = { 8, 7, 1, 3, 5, 2, 4 };
+            int[] sortedTab = CountingSort(tab);
+
+
+            static int[] CountingSort(int[] tab)
+            {
+                int min = tab.Min();
+                int max = tab.Max();
+
+                int[] count = new int[max - min + 1];
+
+                foreach (int num in tab)
+                {
+                    count[num - min]++;
+                }
+
+                int[] sortedTab = new int[tab.Length];
+                int index = 0;
+
+                for (int i = 0; i < count.Length; i++)
+                {
+                    while (count[i] > 0)
+                    {
+                        sortedTab[index++] = i + min;
+                        count[i]--;
+                    }
+                }
+
+                return sortedTab;
+            }
+            string message = string.Join(",", sortedTab);
+            MessageBox.Show(message);
+        }
+    }
+}
